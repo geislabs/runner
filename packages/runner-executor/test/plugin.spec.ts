23cce060c77321bf2987c2ba42b124adf78d9f20
mads hargreave <mads.hargreave@gmail.com>
2021-05-31 11:13:09 +0200

    adad

---
+++ packages/runner-executor/test/plugin.spec.ts
@@ -0,0 +1,31 @@
+import { toArray } from 'ix/asynciterable'
+import { run } from '../lib'
+
+describe('plugins', () => {
+    test('simple', async () => {
+        await expect(
+            toArray(
+                run({
+                    plugins: [{ init: () => ({ values: () => [1, 2, 3] }) }],
+                    input: function* ({ values }) {
+                        yield* values()
+                    },
+                })
+            )
+        ).resolves.toStrictEqual([1, 2, 3])
+    })
+    test('async', async () => {
+        await expect(
+            toArray(
+                run({
+                    plugins: [
+                        { init: async () => ({ values: () => [1, 2, 3] }) },
+                    ],
+                    input: function* ({ values }) {
+                        yield* values()
+                    },
+                })
+            )
+        ).resolves.toStrictEqual([1, 2, 3])
+    })
+})
