0ba0b88e26a47f8e692ac1318138cfdb868495fb
mads hargreave <mads.hargreave@gmail.com>
2021-06-02 08:30:52 +0200

    released

--- packages/runner-main/lib/index.ts
+++ packages/runner-main/lib/index.ts
@@ -1,5 +1,5 @@
 export { Plugin } from '@geislabs/runner-plugin'
-export { config, Executor } from '@geislabs/runner-executor'
+export { config, IExecutor as Executor } from '@geislabs/runner-executor'
 export {
     AnyWorkerConfig as Worker,
     Context,
